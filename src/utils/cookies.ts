cc20143335b1354a75f202dac62e764bc7d4ca45
RightFix <righteousnessude@gmail.com>
2026-05-23 12:18:29 +0100

    user data would be saved in cookie instead of local storage

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -7,14 +7,24 @@ export function getCookie(name: string): string | null {
   return null;
 }
 
+const isProduction = import.meta.env.PROD;
+
 export function setCookie(name: string, value: string, days: number = 7): void {
   const expires = new Date();
   expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-  document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/;SameSite=Lax`;
+  let cookieString = `${name}=${value};expires=${expires.toUTCString()};path=/;SameSite=Lax`;
+  if (isProduction) {
+    cookieString += ';Secure';
+  }
+  document.cookie = cookieString;
 }
 
 export function removeCookie(name: string): void {
-  document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/;SameSite=Lax`;
+  let cookieString = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/;SameSite=Lax`;
+  if (isProduction) {
+    cookieString += ';Secure';
+  }
+  document.cookie = cookieString;
 }
 
 export function setCookieJSON<T>(name: string, value: T, days: number = 7): void {
